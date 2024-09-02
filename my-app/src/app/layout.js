01522be65930ca5d0fce850d3356dc2cf1962abe
Jian Hong Li <lijh0901@gmail.com>
2024-09-02 15:45:52 -0400

    portfolio

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico?v=1" />
       </head>
       <body className="w-dvw h-dvh">{children}</body>
     </html>
